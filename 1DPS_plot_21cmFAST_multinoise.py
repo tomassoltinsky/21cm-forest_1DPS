66945f5ee8b35a78ece73ddfdf30d95a53573a7a
tomassoltinsky <tomas.soltinsky@tifr.res.in>
2024-10-14 18:48:44 +0530

    Comment 1DPS plots

--- 1DPS_plot_21cmFAST_multinoise.py
+++ 1DPS_plot_21cmFAST_multinoise.py
@@ -13,27 +13,25 @@ import scipy.signal as scisi
 from astropy.convolution import convolve, Box1DKernel
 from matplotlib.ticker import AutoMinorLocator
 from matplotlib import gridspec
-import openpyxl
-import time
-
-
 
 fsize = 24
 
-z_name = float(sys.argv[1])
-dvH = float(sys.argv[2])
-spec_res = float(sys.argv[3])
-fX_name = float(sys.argv[4])
-xHI_mean = float(sys.argv[5])
+#Input parameters
+z_name = float(sys.argv[1])   #redshift
+dvH = float(sys.argv[2])      #rebinning width in km/s
+spec_res = float(sys.argv[3]) #spectral resolution of telescope (i.e. frequency channel width) in kHz
+fX_name = float(sys.argv[4])  #log10(f_X)
+xHI_mean = float(sys.argv[5]) #mean x_HI
 n_los = 1000
 
-telescope = ['uGMRT','SKA1-low']
-t_int = [500,50]
-S_min_QSO = [64.2,64.2]#,110.6,110.6]
-alpha_R = [-0.44,-0.44]#,-0.89,-0.89]
+#Observational setup parameters
+telescope = ['uGMRT','SKA1-low']  #telescope
+t_int = [500,50]                  #integration time in hours
+S_min_QSO = [64.2,64.2]           #intrinsic flux of QSO at 147Hz in mJy
+alpha_R = [-0.44,-0.44]           #radio spectral index of QSO
 
 
-
+#Load 21-cm forest 1D PS data
 datafile = str('1DPS_dimensionless/1DPS_signal/power_spectrum_signal_21cmFAST_200Mpc_z%.1f_fX%.2f_xHI%.2f_%dkHz_%dLOS.dat' % (z_name,fX_name,xHI_mean,spec_res,n_los))
 data = np.fromfile(str(datafile),dtype=np.float32)
 Nlos = int(data[0])
@@ -42,30 +40,34 @@ k = data[2:2+n_kbins]
 PS_signal = data[2+n_kbins+0*n_kbins*Nlos:2+n_kbins+1*n_kbins*Nlos]
 PS_signal = np.reshape(PS_signal,(Nlos,n_kbins))[:n_los,:]
 
+#Generate k bins
 d_log_k_bins = 0.25
 log_k_bins = np.arange(-1.0-d_log_k_bins/2.,3.+d_log_k_bins/2.,d_log_k_bins)
 log_k_bins = log_k_bins[1:]
 k_bins = np.power(10.,log_k_bins)
 k_bins_cent = np.power(10.,log_k_bins+d_log_k_bins/2.)[:-1]
-print(k_bins)
 print(k_bins_cent)
+print('%d bins from %.3fMHz^-1 to %.3fMHz^-1' % (len(k),k[1],k[-1]))
+
+#Bin the PS data
 PS_signal_bin = np.empty((n_los,len(k_bins_cent)))
 
-print('%d bins from %.3fMHz^-1 to %.3fMHz^-1' % (len(k),k[1],k[-1]))
 for j in range(len(k_bins_cent)):
   ind = np.where((k>=k_bins[j]) & (k<k_bins[j+1]))[0]
-  print(len(ind))
+  print('Number of elements in %.3fMHz^-1<k<%.3fMHz^-1: %d' % (k_bins[j],k_bins[j+1],len(ind)))
 
 for i in range(n_los):
   for j in range(len(k_bins_cent)):
     ind = np.where((k>=k_bins[j]) & (k<k_bins[j+1]))[0]
     PS_signal_bin[i,j] = np.mean(PS_signal[i,ind])
 
+#Take median and 68% scatter for each k bin
 PS_signal_med = np.mean(PS_signal_bin,axis=0)
 PS_signal_16 = np.percentile(PS_signal_bin,16,axis=0)
 PS_signal_84 = np.percentile(PS_signal_bin,84,axis=0)
 
 
+#Do the same for the noise for all observational setups
 Nlos_noise = 1000
 PS_noise_bin = np.empty((len(t_int),Nlos_noise,len(k_bins_cent)))
 PS_noise_med = np.empty((len(t_int),len(k_bins_cent)))
@@ -89,10 +91,9 @@ for i in range(len(t_int)):
   PS_noise_med[i,:] = np.mean(PS_noise_bin[i],axis=0)
   PS_noise_16[i,:]  = np.percentile(PS_noise_bin[i],16,axis=0)
   PS_noise_84[i,:]  = np.percentile(PS_noise_bin[i],84,axis=0)
-  print(t_int[i],np.amin(PS_noise_med[i]),np.amax(PS_noise_med[i]))
 
-print(PS_noise_med)
 
+#Plot the 1D PS
 fig = plt.figure(figsize=(10.,5.))
 gs = gridspec.GridSpec(1,1)
 
@@ -105,18 +106,10 @@ ax0.legend(frameon=False,loc='upper left',fontsize=fsize-3,ncol=2)
 ax0.plot(k_bins_cent,PS_signal_med,'-',color='darkorange',label=r'Signal')
 ax0.fill_between(k_bins_cent,PS_signal_16,PS_signal_84,alpha=0.25,color='darkorange')
 for j in range(0,len(S_min_QSO)):
-  #ax0.plot([k_bins_cent[0],k_bins_cent[-1]],[np.amax(PS_noise_bin[j]),np.amax(PS_noise_bin[j])],'--',color='fuchsia',label=r'Noise')
   ax0.plot(k_bins_cent,PS_noise_med[j],'--',color='fuchsia',label=r'Signal')
-  #ax0.text(20,1.2*np.amax(PS_noise_med[j]),r'$S_{147\mathrm{MHz}}=%.1f\,\mathrm{mJy},\ \alpha_{\mathrm{R}}=%.2f,\, t_{\mathrm{int}}=%d\mathrm{hr}$' % (S_min_QSO[j],alpha_R[j],t_int[j]),fontsize=fsize-4)
   ax0.text(k_bins_cent[3],0.575*PS_noise_med[j,3],r'$\mathrm{%s,}\, t_{\mathrm{int}}=%d\mathrm{hr}$' % (telescope[j],t_int[j]),rotation=21,fontsize=fsize-3)
-  #ax0.text(60,1.3*np.amax(PS_noise_med[j]),r'$\sigma_{\mathrm{Noise}}=0.0021$',fontsize=fsize)
-  #ax0.fill_between(k_bins_cent,PS_noise_16[j],PS_noise_84[j],alpha=0.25,color='fuchsia')
 
-
-
-#ax0.set_xlim(z_min,z_max)
 ax0.set_ylim(6e-9,5e-5)
-#ax0.set_yticks(np.arange(0.97,1.031,0.01))
 ax0.set_xscale('log')
 ax0.set_yscale('log')
 ax0.set_xlabel(r'$k \,\rm [MHz^{-1}]$', fontsize=fsize)
@@ -127,10 +120,8 @@ ax0.tick_params(axis='y',which='major',direction='in',bottom=True,top=True,left=
 		,length=20,width=2,labelsize=fsize)
 ax0.tick_params(axis='both',which='minor',direction='in',bottom=True,top=True,left=True,right=True
 		,length=10,width=2)
-#ax0.set_title(r'$\mathrm{log}(f_{\rm X})=%.1f,\ \langle x_{\rm HI}\rangle =%.2f$' % (fX_name,xHI_mean),fontsize=fsize)
-#ax0.set_title(r'$\mathrm{log}(f_{\rm X})=%.1f,\ \langle x_{\rm HI}\rangle =%.2f,\, S_{147\mathrm{MHz}}=%.1f\,\mathrm{mJy},\ \alpha_{\mathrm{R}}=%.2f$' % (fX_name,xHI_mean,S_min_QSO[0],alpha_R[0]),fontsize=fsize)
 
 plt.tight_layout()
 plt.subplots_adjust(hspace=.0)
-plt.savefig('1DPS_plots/kP21_21cmFAST_multinoise_200Mpc_z%.1f_fX%.2f_xHI%.2f_%dkHz.pdf' % (z_name,fX_name,xHI_mean,spec_res))
+plt.savefig('plots/kP21_21cmFAST_multinoise_200Mpc_z%.1f_fX%.2f_xHI%.2f_%dkHz.png' % (z_name,fX_name,xHI_mean,spec_res))
 plt.show()
--- 1DPS_plot_21cmFAST_novpec_200Mpc.py
+++ 1DPS_plot_21cmFAST_novpec_200Mpc.py
@@ -21,22 +21,23 @@ import PS1D
 
 fsize = 24
 
-z_name = float(sys.argv[1])
-dvH = float(sys.argv[2])
-logfX = float(sys.argv[3])
-xHI_mean = float(sys.argv[4])
+#Input parameters
+z_name = float(sys.argv[1])   #redshift
+dvH = float(sys.argv[2])      #rebinning width in km/s
+logfX = float(sys.argv[3])    #log10(f_X)
+xHI_mean = float(sys.argv[4]) #<x_HI>
 path = '../../datasets/21cmFAST_los/'
 n_los = 1000
 Nlos = 1000
 
+#Generate k bins
 d_log_k_bins = 0.25
 log_k_bins = np.arange(-1.0-d_log_k_bins/2.,10.+d_log_k_bins/2.,d_log_k_bins)
 k_bins = np.power(10.,log_k_bins)
 k_bins_cent = np.power(10.,log_k_bins+d_log_k_bins/2.)[:-1]
-#print(k_bins)
-#print(k_bins_cent)
 PS_signal_bin = np.empty((n_los,len(k_bins_cent)))
 
+#Load data for x-axis and turn to frequency and redshift axis
 datafile = str('%stau_long/los_200Mpc_n%d_z%.3f_dv%d.dat' %(path,Nlos,z_name,dvH))
 data  = np.fromfile(str(datafile),dtype=np.float32)
 z     = data[0]	#redshift
@@ -48,6 +49,7 @@ freq_ori = instrumental_features.freq_obs(z,vel_axis*1e5)
 bandwidth = (freq_ori[-1]-freq_ori[0])/1e6
 print('Bandwidth = %.2fMHz' % bandwidth)
 
+#Load optical depth data
 datafile = str('%stau_long/tau_200Mpc_n%d_z%.3f_fX%.1f_xHI%.2f_dv%d.dat' %(path,Nlos,z_name,logfX,xHI_mean,dvH))
 data = np.fromfile(str(datafile),dtype=np.float32)
 tau = np.reshape(data,(Nlos,-1))
@@ -61,22 +63,17 @@ n_kbins = int((len(freq_uni)/2+1))
 
 PS_signal = np.empty((n_los,n_kbins))
 
+#Compute the 1D power spectrum
 for j in range(n_los):
-
-  #freq_smooth,signal_smooth = instrumental_features.smooth_fixedbox(freq_uni,signal_uni[j],spec_res)
-  #noise = instrumental_features.add_noise(freq_smooth,telescope,spec_res,S_min_QSO,alpha_R,t_int,N_d)
-  #freq_smooth = freq_smooth[:-1]
-  #signal_smooth = signal_smooth[:-1]
   k,PS_signal[j,:] = PS1D.get_P(signal_uni[j],bandwidth)
 
-for j in range(len(k_bins_cent)):
-  ind = np.where((k>=k_bins[j]) & (k<k_bins[j+1]))[0]
-
+#Bin the PS data
 for i in range(n_los):
   for j in range(len(k_bins_cent)):
     ind = np.where((k>=k_bins[j]) & (k<k_bins[j+1]))[0]
     PS_signal_bin[i,j] = np.mean(PS_signal[i,ind])
 
+#Take median and 68% scatter for each k bin
 PS_signal_med = np.median(PS_signal_bin,axis=0)
 PS_signal_16 = np.percentile(PS_signal_bin,16,axis=0)
 PS_signal_84 = np.percentile(PS_signal_bin,84,axis=0)
@@ -85,6 +82,7 @@ print('%d bins from %.3fMHz^-1 to %.3fMHz^-1' % (len(k),k[1],k[-1]))
 
 
 
+#Do the same for the case of no peculiar velocity
 datafile = str('%stau_long/tau_200Mpc_n%d_z%.3f_fX%.1f_xHI%.2f_dv%d_novpec.dat' %(path,Nlos,z_name,logfX,xHI_mean,dvH))
 data = np.fromfile(str(datafile),dtype=np.float32)
 tau = np.reshape(data,(Nlos,-1))
@@ -98,17 +96,9 @@ signal_uni = signal_uni[:,:-1]
 PS_signal = np.empty((n_los,n_kbins))
 
 for j in range(n_los):
-
-  #freq_smooth,signal_smooth = instrumental_features.smooth_fixedbox(freq_uni,signal_uni[j],spec_res)
-  #noise = instrumental_features.add_noise(freq_smooth,telescope,spec_res,S_min_QSO,alpha_R,t_int,N_d)
-  #freq_smooth = freq_smooth[:-1]
-  #signal_smooth = signal_smooth[:-1]
   k,PS_signal[j,:] = PS1D.get_P(signal_uni[j],bandwidth)
 
 PS_signal_bin = np.empty((n_los,len(k_bins_cent)))
-
-for j in range(len(k_bins_cent)):
-  ind = np.where((k>=k_bins[j]) & (k<k_bins[j+1]))[0]
 
 for i in range(n_los):
   for j in range(len(k_bins_cent)):
@@ -120,7 +110,7 @@ PS_signal_novpec_16 = np.percentile(PS_signal_bin,16,axis=0)
 PS_signal_novpec_84 = np.percentile(PS_signal_bin,84,axis=0)
 
 
-
+#Plot the 1D power spectrum
 fig = plt.figure(figsize=(10.,5.))
 gs = gridspec.GridSpec(1,1)
 
@@ -142,9 +132,7 @@ print(k_max)
 ax0.plot([k_max,k_max],[1e-30,1e0],'--',c='fuchsia')
 ax0.text(k_max*1.1,1e-19,r'$\Delta\nu=%d\,\rm kHz$ limit' % spec_res,rotation='vertical',fontsize=fsize)
 
-#ax0.set_xlim(z_min,z_max)
 ax0.set_ylim(1e-9,1e-5)
-#ax0.set_yticks(np.arange(0.97,1.031,0.01))
 ax0.set_xscale('log')
 ax0.set_yscale('log')
 ax0.set_xlabel(r'$k \,\rm [MHz^{-1}]$', fontsize=fsize)
@@ -155,9 +143,8 @@ ax0.tick_params(axis='y',which='major',direction='in',bottom=True,top=True,left=
 		,length=20,width=2,labelsize=fsize)
 ax0.tick_params(axis='both',which='minor',direction='in',bottom=True,top=True,left=True,right=True
 		,length=10,width=2)
-#ax0.set_title(r'$\mathrm{log}(f_{\rm X})=%.1f,\ \langle x_{\rm HI}\rangle =%.2f$' % (fX_name,xHI_mean),fontsize=fsize)
 
 plt.tight_layout()
 plt.subplots_adjust(bottom=.2)
-plt.savefig('1DPS_plots/power_spectrum_21cmFAST_dimles_novpec_200Mpc_z%.1f_fX%.2f_xHI%.2f.pdf' % (z_name,logfX,xHI_mean))
+plt.savefig('plots/power_spectrum_21cmFAST_dimles_novpec_200Mpc_z%.1f_fX%.2f_xHI%.2f.png' % (z_name,logfX,xHI_mean))
 plt.show()
