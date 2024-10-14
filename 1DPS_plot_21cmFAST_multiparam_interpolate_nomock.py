77b6b5d891d443f25bce25fb84e7d23760653eae
tomassoltinsky <tomas.soltinsky@tifr.res.in>
2024-10-14 18:48:44 +0530

    Comment 1DPS plots

--- 1DPS_plot_21cmFAST_multiparam_interpolate_nomock.py
+++ 1DPS_plot_21cmFAST_multiparam_interpolate_nomock.py
@@ -14,71 +14,32 @@ import scipy.signal as scisi
 from astropy.convolution import convolve, Box1DKernel
 from matplotlib.ticker import AutoMinorLocator
 from matplotlib import gridspec
-import openpyxl
-import time
 from scipy import interpolate
-
-
 
 fsize = 24
 
-z_name = float(sys.argv[1])
-dvH = float(sys.argv[2])
-spec_res = float(sys.argv[3])
+#Input parameters
+z_name = float(sys.argv[1])   #redshift
+dvH = float(sys.argv[2])      #rebinning width in km/s
+spec_res = float(sys.argv[3]) #spectral resolution of telescope (i.e. frequency channel width) in kHz
 n_los = 1000
 
-fX_fid = float(sys.argv[4])
-xHI_fid = float(sys.argv[5])
-
-spec_res = 8
-S147 = 64.2
-alphaR = -0.44
-tint = 500
-telescope = 'uGMRT'
+fX_fid = float(sys.argv[4])   #fiducial log(f_X)
+xHI_fid = float(sys.argv[5])  #fiducial <x_HI>
 
 #Find all of the datasets for the interpolation
-path_LOS = '../../datasets/21cmFAST_los/los/'
+path_LOS = 'datasets/21cmFAST_los/los/'
 files = glob.glob(path_LOS+'*.dat')
 
+#Remove files if needed
 files_to_remove = glob.glob(path_LOS+'*fXnone*.dat')
 for i in range(len(files_to_remove)):
    files.remove(files_to_remove[i])
-'''
-files_to_remove = glob.glob(path_LOS+'*xHI1.*.dat')
-for i in range(len(files_to_remove)):
-   files.remove(files_to_remove[i])
-files_to_remove = glob.glob(path_LOS+'*xHI0.9*.dat')
-for i in range(len(files_to_remove)):
-   files.remove(files_to_remove[i])
-files_to_remove = glob.glob(path_LOS+'*xHI0.8*.dat')
-for i in range(len(files_to_remove)):
-   files.remove(files_to_remove[i])
-files_to_remove = glob.glob(path_LOS+'*xHI0.7*.dat')
-for i in range(len(files_to_remove)):
-   files.remove(files_to_remove[i])
-files_to_remove = glob.glob(path_LOS+'*xHI0.69*.dat')
-for i in range(len(files_to_remove)):
-   files.remove(files_to_remove[i])
-files_to_remove = glob.glob(path_LOS+'*xHI0.68*.dat')
-for i in range(len(files_to_remove)):
-   files.remove(files_to_remove[i])
-files_to_remove = glob.glob(path_LOS+'*xHI0.67*.dat')
-for i in range(len(files_to_remove)):
-   files.remove(files_to_remove[i])
-files_to_remove = glob.glob(path_LOS+'*xHI0.66*.dat')
-for i in range(len(files_to_remove)):
-   files.remove(files_to_remove[i])
-files_to_remove = glob.glob(path_LOS+'*xHI0.65*.dat')
-for i in range(len(files_to_remove)):
-   files.remove(files_to_remove[i])
-files_to_remove = glob.glob(path_LOS+'*xHI0.64*.dat')
-for i in range(len(files_to_remove)):
-   files.remove(files_to_remove[i])
-'''
 
 logfX = np.empty(len(files))
 xHI_mean = np.empty(len(files))
 
+#Generate k bins
 d_log_k_bins = 0.25
 log_k_bins = np.arange(-1.0-d_log_k_bins/2.,3.+d_log_k_bins/2.,d_log_k_bins)
 log_k_bins = log_k_bins[1:]
@@ -88,7 +49,7 @@ k_bins_cent = np.power(10.,log_k_bins+d_log_k_bins/2.)[:-1]
 PS_signal_sim = np.empty((len(files),len(k_bins_cent)))
 
 
-
+#Load and prepare fiducial data
 datafile = str('1DPS_dimensionless/1DPS_signal/power_spectrum_signal_21cmFAST_200Mpc_z%.1f_fX%.2f_xHI%.2f_%dkHz_%dLOS.dat' % (z_name,fX_fid,xHI_fid,spec_res,1000))
 data = np.fromfile(str(datafile),dtype=np.float32)
 Nlos = int(data[0])
@@ -106,30 +67,6 @@ for i in range(n_los):
 
 PS_signal_fid = np.median(PS_signal_bin,axis=0)
 
-
-'''
-#Read data for noise
-Nlos_noise = 500
-datafile = str('1DPS_dimensionless/1DPS_noise/power_spectrum_noise_50Mpc_z%.1f_%s_%dkHz_Smin%.1fmJy_alphaR%.2f_t%dh.dat' % (z_name,telescope,spec_res,S147,alphaR,tint))
-data = np.fromfile(str(datafile),dtype=np.float32)
-n_kbins = int(data[0])
-k = data[1:1+n_kbins]
-PS_noise = data[1+n_kbins+0*n_kbins*Nlos_noise:1+n_kbins+1*n_kbins*Nlos_noise]
-PS_noise = np.reshape(PS_noise,(Nlos_noise,n_kbins))
-
-#Bin the PS data
-PS_noise_bin = np.empty((Nlos_noise,len(k_bins_cent)))
-
-for i in range(Nlos_noise):
-  for l in range(len(k_bins_cent)):
-    ind = np.where((k>=k_bins[l]) & (k<k_bins[l+1]))[0]
-    PS_noise_bin[i,l] = np.mean(PS_noise[i,ind])
-
-#Take median for each k bin
-#PS_noise = np.mean(PS_noise_bin,axis=0)
-sig_PS_noise = np.std(PS_noise_bin,axis=0)
-'''
-
 done_perc = 0.1
 #Find all of the parameter values in simulated data and read the data for interpolation
 for j in range(len(files)):
@@ -137,7 +74,6 @@ for j in range(len(files)):
     data = np.fromfile(str(files[j]),dtype=np.float32)
     logfX[j] = data[9]
     xHI_mean[j] = data[11]
-    #print('f_X=%.2f, <x_HI,box>=%.8f' % (logfX[j],xHI_mean[j]))
 
     #Read data for signal
     datafile = str('1DPS_dimensionless/1DPS_signal/power_spectrum_signal_21cmFAST_200Mpc_z%.1f_fX%.2f_xHI%.2f_%dkHz_%dLOS.dat' % (z_name,logfX[j],xHI_mean[j],spec_res,Nlos))
@@ -165,22 +101,18 @@ for j in range(len(files)):
       print('Done %.2f' % done_files)
       done_perc = done_perc+0.1
 
-#Set up N-dimensional linear interpolator for calculating P21 for any parameter values within the range given in the prior function
+#Set up interpolator
 inter_fun_PS21 = interpolate.LinearNDInterpolator(np.transpose([xHI_mean,logfX]),PS_signal_sim)
 
 
-
-
+#Plot varying x_HI
 xHI_interpol = np.arange(0.01,0.99,0.0002)
 colours = plt.cm.viridis(np.linspace(0,1,len(xHI_interpol)))
 
 fig, (ax0, cbar_ax) = plt.subplots(ncols=2,figsize=(10.,5.),gridspec_kw={'width_ratios': [9.75,0.25]})
-#gs = gridspec.GridSpec(1,1)
-
-#ax0 = plt.subplot(gs[0,0])
 
 cmap = plt.cm.viridis_r
-norm = plt.Normalize(vmin=0., vmax=1.)#np.amax(xHI_interpol))
+norm = plt.Normalize(vmin=0., vmax=1.)
 cb1 = mpl.colorbar.ColorbarBase(cbar_ax, cmap=cmap, norm=norm, orientation='vertical')
 cbar_ax.tick_params(labelsize=fsize)
 cb1.set_label(r'$\langle x_{\rm HI}\rangle$',fontsize=fsize)
@@ -189,15 +121,8 @@ for i in range(len(xHI_interpol)):
    
    PS_signal_inter = inter_fun_PS21(xHI_interpol[i], fX_fid)
    ax0.plot(k_bins_cent,PS_signal_inter,'-',color=cmap(norm(xHI_interpol[i])),label=r'Signal')
-   #ax0.text(1.1*k_bins_cent[-1],PS_signal_sim[j,-1],r'$%.2f$' % (xHI_mean[j]),fontsize=fsize-4)
 
-
-
-
-
-#ax0.set_xlim(0.8,4e2)
 ax0.set_ylim(1e-9,1e-4)
-#ax0.set_yticks(np.arange(0.97,1.031,0.01))
 ax0.set_xscale('log')
 ax0.set_yscale('log')
 ax0.set_xlabel(r'$k \,\rm [MHz^{-1}]$', fontsize=fsize)
@@ -209,7 +134,6 @@ ax0.tick_params(axis='y',which='major',direction='in',bottom=True,top=True,left=
 ax0.tick_params(axis='both',which='minor',direction='in',bottom=True,top=True,left=True,right=True
 		,length=10,width=2)
 
-#ax0.errorbar(k_bins_cent,PS_signal_fid,yerr=sig_PS_noise,fmt=' ',marker='o',capsize=5,color='royalblue')
 ax0.text(3.5,4e-9,r'$\mathrm{log}_{10}(f_{\mathrm{X}})=%.1f$' % (fX_fid),fontsize=fsize-3)
 
 plt.tight_layout()
@@ -219,13 +143,11 @@ plt.show()
 
 
 
+#Plot varying log(f_X)
 logfX_interpol = np.arange(-4.,1.0001,0.001)
 colours = plt.cm.inferno(np.linspace(0,1,len(logfX_interpol)))
 
 fig, (ax0, cbar_ax) = plt.subplots(ncols=2,figsize=(10.,5.),gridspec_kw={'width_ratios': [9.75,0.25]})
-#gs = gridspec.GridSpec(1,1)
-
-#ax0 = plt.subplot(gs[0,0])
 
 cmap = plt.cm.inferno
 norm = plt.Normalize(vmin=np.amin(logfX_interpol), vmax=np.amax(logfX_interpol))
@@ -237,13 +159,10 @@ for i in range(len(logfX_interpol)):
    
    PS_signal_inter = inter_fun_PS21(xHI_fid, logfX_interpol[i])
    ax0.plot(k_bins_cent,PS_signal_inter,'-',color=cmap(norm(logfX_interpol[i])),label=r'Signal')
-   #ax0.text(1.1*k_bins_cent[-1],PS_signal_sim[j,-1],r'$%.1f$' % (logfX[j]),fontsize=fsize-4)
 
 ax0.text(3.5,1e-14,r'$\langle x_{\mathrm{HI}}\rangle=%.2f$' % (xHI_fid),fontsize=fsize-3)
 
-#ax0.set_xlim(0.8,4e2)
 ax0.set_ylim(5e-16,3e-5)
-#ax0.set_yticks(np.arange(0.97,1.031,0.01))
 ax0.set_xscale('log')
 ax0.set_yscale('log')
 ax0.set_xlabel(r'$k \,\rm [MHz^{-1}]$', fontsize=fsize)
@@ -254,8 +173,6 @@ ax0.tick_params(axis='y',which='major',direction='in',bottom=True,top=True,left=
 		,length=20,width=2,labelsize=fsize)
 ax0.tick_params(axis='both',which='minor',direction='in',bottom=True,top=True,left=True,right=True
 		,length=10,width=2)
-
-#ax0.errorbar(k_bins_cent,PS_signal_fid,yerr=sig_PS_noise,fmt=' ',marker='o',capsize=5,color='royalblue')
 
 plt.tight_layout()
 plt.subplots_adjust(hspace=2.0)
