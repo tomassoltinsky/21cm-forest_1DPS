9e75c03131e342d375f5d3cdd27e88fabca8730d
tomassoltinsky <tomas.soltinsky@tifr.res.in>
2024-10-14 17:57:43 +0530

    Comment 1DPS data

--- 1DPS_data_noise.py
+++ 1DPS_data_noise.py
@@ -1,40 +1,36 @@
 """
-Generating 21cm forest 1D PS data for noise.
+Generating 21cm forest 1D PS data for noise only.
 
 Version 19.10.2023
 """
 
 import sys
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import scipy.signal as scisi
 from astropy.convolution import convolve, Box1DKernel
-from matplotlib.ticker import AutoMinorLocator
-from matplotlib import gridspec
-import openpyxl
 import time
-
-start_clock = time.perf_counter()
-
-#constants
 
 import instrumental_features
 import PS1D
 
-path = '../../datasets/21cmFAST_los/'
-z_name = float(sys.argv[1])
-dvH = float(sys.argv[2])
-telescope = str(sys.argv[3])
-spec_res = float(sys.argv[4])
-S147 = float(sys.argv[5])
-alphaR = float(sys.argv[6])
-N_d = float(sys.argv[7])
-tint = float(sys.argv[8])
+start_clock = time.perf_counter()
+
+#Input parameters
+path = 'datasets/21cmFAST_los/'
+
+z_name = float(sys.argv[1])		#redshift
+dvH = float(sys.argv[2])		  #rebinning width in km/s
+spec_res = float(sys.argv[3])	#spectral resolution of telescope (i.e. frequency channel width) in kHz
+telescope = str(sys.argv[4])	#telescope
+N_d = int(sys.argv[5])			  #number dishes used by telescope
+S_min_QSO = float(sys.argv[6])	    #intrinsic flux of QSO at 147Hz in mJy
+alpha_R = float(sys.argv[7])	  #radio spectral index of QSO
+t_int = float(sys.argv[8])		  #integration time of observation in hours
+
 n_los = 1000
 fX_name = -2.
 mean_xHI = 0.25
 
+#Load data for velocity-axis and turn to frequency and redshift axis
 datafile = str('%stau_long/los_200Mpc_n%d_z%.3f_dv%d.dat' %(path,1000,z_name,dvH))
 data  = np.fromfile(str(datafile),dtype=np.float32)
 z     = data[0]	#redshift
@@ -44,6 +40,7 @@ x_initial = 4
 vel_axis = data[(x_initial+Nbins):(x_initial+2*Nbins)]#Hubble velocity along LoS in km/s
 freq_ori = instrumental_features.freq_obs(z,vel_axis*1e5)
 
+#Incorporate spectral resolution of telescope
 freq_uni = instrumental_features.uni_freq(freq_ori,np.array([freq_ori]))[0]
 freq_smooth = instrumental_features.smooth_fixedbox(freq_uni,freq_uni,spec_res)[0]
 freq_smooth = freq_smooth[:-1]
@@ -55,9 +52,10 @@ PS_noise = np.empty((n_los,n_kbins))
 
 done_perc = 0.1
 
+#Compute the noise for each LOS and calculate 1D power spectrum
 for j in range(n_los):
 
-  noise = instrumental_features.add_noise(freq_smooth,telescope,spec_res,S147,alphaR,tint,N_d)
+  noise = instrumental_features.add_noise(freq_smooth,telescope,spec_res,S_min_QSO,alpha_R,t_int,N_d)
   k,PS_noise[j,:] = PS1D.get_P(1.+noise,bandwidth)
 
   done_LOS = (j+1)/n_los
@@ -65,10 +63,11 @@ for j in range(n_los):
     print('Done %.2f' % done_LOS)
     done_perc = done_perc+0.1
 
+#Write data
 array = np.append(n_los,n_kbins)
 array = np.append(array,k)
 array = np.append(array,PS_noise)
-array.astype('float32').tofile('1DPS_dimensionless/1DPS_noise/power_spectrum_noise_21cmFAST_200Mpc_z%.1f_%s_%dkHz_t%dh_Smin%.1fmJy_alphaR%.2f_%dLOS.dat' % (z_name,telescope,spec_res,tint,S147,alphaR,n_los),sep='')
+array.astype('float32').tofile('1DPS_dimensionless/1DPS_noise/power_spectrum_noise_21cmFAST_200Mpc_z%.1f_%s_%dkHz_t%dh_Smin%.1fmJy_alphaR%.2f_%dLOS.dat' % (z_name,telescope,spec_res,t_int,S_min_QSO,alpha_R,n_los),sep='')
 
 stop_clock = time.perf_counter()
 time_taken = (stop_clock-start_clock)
--- 1DPS_data_signal_21cmFAST_multiparam.py
+++ 1DPS_data_signal_21cmFAST_multiparam.py
@@ -1,5 +1,5 @@
 """
-Generating 21cm forest 1D PS data for signal+noise.
+Generating 21cm forest 1D PS data for signal.
 
 Version 26.04.2024
 """
@@ -7,47 +7,25 @@ Version 26.04.2024
 import sys
 import glob, os
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import scipy.signal as scisi
 from astropy.convolution import convolve, Box1DKernel
-from matplotlib.ticker import AutoMinorLocator
-from matplotlib import gridspec
-import openpyxl
 import time
-
-start_clock = time.perf_counter()
-
-#constants
 
 import instrumental_features
 import PS1D
 
+start_clock = time.perf_counter()
 
-
-#path = 'data/'
-path = '../../datasets/21cmFAST_los/'
-z_name = float(sys.argv[1])
-dvH = float(sys.argv[2])
-spec_res = float(sys.argv[3])
-n_los = int(sys.argv[4])
+#Input parameters
+path = 'datasets/21cmFAST_los/'
+z_name = float(sys.argv[1])	  #redshift
+dvH = float(sys.argv[2])		  #rebinning width in km/s
+spec_res = float(sys.argv[3])	#spectral resolution of telescope (i.e. frequency channel width) in kHz
+n_los = int(sys.argv[4])      #number of lines-of-sight
 
 #Find all of the datasets for the interpolation
 files = glob.glob(path+'los/*.dat')
-print('Number of files: %d' % len(files))
-files_to_remove = glob.glob(path+'los/*xHI0.0*.dat')
-for i in range(len(files_to_remove)):
-   files.remove(files_to_remove[i])
-files_to_remove = glob.glob(path+'los/*xHI0.1*.dat')
-for i in range(len(files_to_remove)):
-   files.remove(files_to_remove[i])
-files_to_remove = glob.glob(path+'los/*xHI0.2*.dat')
-for i in range(len(files_to_remove)):
-   files.remove(files_to_remove[i])
-files_to_remove = glob.glob(path+'los/*xHI0.3*.dat')
-for i in range(len(files_to_remove)):
-   files.remove(files_to_remove[i])
-files_to_remove = glob.glob(path+'los/*xHI0.4*.dat')
+#Remove some if needed
+files_to_remove = glob.glob(path+'los/*fXnone*.dat')
 for i in range(len(files_to_remove)):
    files.remove(files_to_remove[i])
 
@@ -64,6 +42,7 @@ for j in range(len(files)):
 
 Nlos = 1000
 
+#Load data for velocity-axis and turn to frequency and redshift axis
 datafile = str('%stau_long/los_200Mpc_n%d_z%.3f_dv%d.dat' %(path,Nlos,z_name,dvH))
 data  = np.fromfile(str(datafile),dtype=np.float32)
 z     = data[0]	#redshift
@@ -73,6 +52,7 @@ x_initial = 4
 vel_axis = data[(x_initial+Nbins):(x_initial+2*Nbins)]#Hubble velocity along LoS in km/s
 freq_ori = instrumental_features.freq_obs(z,vel_axis*1e5)
 
+#Incorporate spectral resolution of telescope
 Nbins = len(freq_ori)
 print('Number of pixels (original): %d' % Nbins)
 freq_uni = instrumental_features.uni_freq(freq_ori,np.array([freq_ori]))[0]
@@ -82,17 +62,19 @@ print('Number of pixels (smoothed): %d' % len(freq_smooth))
 print('Bandwidth = %.2fMHz' % bandwidth)
 n_kbins = int((len(freq_smooth)/2+1))
 
+#Calculate 1D power spectrum for all parameter values
 for l in range(len(xHI_mean)):
 
   print('<xHI>=%.2f, log10(fX)=%.2f, %d/%d' % (xHI_mean[l],logfX[l],l+1,len(xHI_mean)))
 
+  #Load data for optical depth and turn to signal
   datafile = str('%stau_long/tau_200Mpc_n%d_z%.3f_fX%.1f_xHI%.2f_dv%d.dat' %(path,Nlos,z_name,logfX[l],xHI_mean[l],dvH))
   data = np.fromfile(str(datafile),dtype=np.float32)
   tau = np.reshape(data,(Nlos,Nbins))
 
   tau = tau[:n_los,:]
   signal_ori = instrumental_features.transF(tau)
-  freq_uni,signal_uni = instrumental_features.uni_freq(freq_ori,signal_ori)
+  freq_uni,signal_uni = instrumental_features.uni_freq(freq_ori,signal_ori) #Interpolate signal to uniform frequency grid
 
   PS_signal = np.empty((n_los,n_kbins))
 
@@ -100,17 +82,17 @@ for l in range(len(xHI_mean)):
 
   for j in range(n_los):
 
-    freq_smooth,signal_smooth = instrumental_features.smooth_fixedbox(freq_uni,signal_uni[j],spec_res)
-    #noise = instrumental_features.add_noise(freq_smooth,telescope,spec_res,S_min_QSO,alpha_R,t_int,N_d)
+    freq_smooth,signal_smooth = instrumental_features.smooth_fixedbox(freq_uni,signal_uni[j],spec_res) #Incorporate spectral resolution of telescope
     freq_smooth = freq_smooth[:-1]
     signal_smooth = signal_smooth[:-1]
-    k,PS_signal[j,:] = PS1D.get_P(signal_smooth,bandwidth)
+    k,PS_signal[j,:] = PS1D.get_P(signal_smooth,bandwidth) #Calculate 1D power spectrum
 
     done_LOS = (j+1)/n_los
     if done_LOS>=done_perc:
       print('Done %.2f' % done_LOS)
       done_perc = done_perc+0.1
       
+  #Write data
   array = np.append(n_los,n_kbins)
   array = np.append(array,k)
   array = np.append(array,PS_signal)
